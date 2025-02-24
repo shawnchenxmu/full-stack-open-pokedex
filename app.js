7b89680f19a4dd43e48f095bec33384b1a050fc9
chensheng <1004740775@qq.com>
2025-02-24 14:59:01 +0800

    feat: test failed case

--- app.js
+++ app.js
@@ -11,5 +11,8 @@ app.get('/version', (req, res) => {
 })
 
 app.listen(PORT, () => {
+  this_causes_error
+  // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
+
 })
